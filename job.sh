182f267c157929ba21362401e72a007ad1f4a725
Enrico Tirri <enrico.tirri@mail.polimi.it>
2024-06-04 19:00:30 +0200

    Update workflow

--- job.sh
+++ job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=MatrixTestNegroTirri
 #SBATCH --output=testMatrixOut.txt
-#SBATCH --time=02:00
+#SBATCH --time=00:20
 #SBATCH --ntasks=2
-#SBATCH --cpus-per-task=1
 
-srun singularity run --bind "$TMPDIR" matrix_multiplication.sif
+export HWLOC_COMPONENTS=-gl
+singularity run --bind "$TMPDIR" matrix_multiplication.sif
